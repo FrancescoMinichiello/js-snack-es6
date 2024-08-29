6b26d1d9ab29199808ca46fc21dd168ed71d4c43
FrancescoMinichiello <fraps2278@gmail.com>
2024-08-29 14:54:57 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+/*
+SNACK 1
+1- creare un'array con il nome del tavolo
+2- inserire nell'array la lista degli invitati
+(nome posto occupato)
+2.1 creare un array che conterrà i posti
+del tavolo
+*/
+
+const tableVip = [
+    place1 = {
+        id: 1,
+        name: 'Brad Pitt',
+        table: 'vip'
+    },
+    place2 = {
+        id: 2,
+        name: 'Johnny Depp',
+        table: 'vip'
+    },
+    place3 = {
+        id: 3,
+        name: 'Lady Gaga',
+        table: 'vip'
+    },
+    place4 = {
+        id: 4,
+        name: 'Cristiano Ronaldo',
+        table: 'vip'
+    },
+    place5 = {
+        id: 5,
+        name: 'Georgina Rodriguez',
+        table: 'vip'
+    },
+    place6 = {
+        id: 6,
+        name: 'Chiara Ferragni',
+        table: 'vip'
+    },
+    place7 = {
+        id: 7,
+        name: 'George Clooney',
+        table: 'vip'
+    },
+    place8 = {
+        id: 8,
+        name: 'Amal Clooney',
+        table: 'vip'
+    },
+    place9 = {
+        id: 9,
+        name: 'Fedez',
+        table: 'vip'
+    },
+    place10 = {
+        id: 10,
+        name: 'Amadeus',
+        table: 'vip'
+    },
+    place11 = {
+        id: 11,
+        name: 'Fiorello',
+        table: 'vip'
+    },
+
+]
+
+console.table(tableVip)
