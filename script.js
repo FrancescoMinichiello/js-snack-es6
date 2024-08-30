3b35f0d4d7878ecd74b28a20217f10a193b4ce49
FrancescoMinichiello <fraps2278@gmail.com>
2024-08-30 16:06:56 +0200

    start Snack 4

--- script.js
+++ script.js
@@ -75,21 +75,97 @@ console.log(idOver120)
 Snack 3
  1 - dichiarare un array di oggetti
  2 - inserire nell'array 2 proprietà : nome / peso
- 3 - creare un ciclo che gira tra gli oggetti
  4 - stampare in console  
  */
 
+
+/*
 const bikes = [
-    { name: "Wilier Zero SLR", weight: "6,8kg" },
-    { name: "Orbea Orca M10iLTD PWR", weight: "6,64kg" },
-    { name: "Look 795 Blade RS", weight: "7kg" },
-    { name: "Cube Litening Air C:68X SLT", weight: "6,6kg" }
+   { name: "Wilier Zero SLR", weight: "6" },
+   { name: "Orbea Orca M10iLTD PWR", weight: "9" },
+   { name: "Look 795 Blade RS", weight: "7" },
+   { name: "Cube Litening Air C:68X SLT", weight: "5" }
 ]
 
 let lightBicycle = bikes[0];
-console.log(lightBicycle)
+
+
+//3 - creare un ciclo che gira tra gli oggetti
+
+for (const bike of bikes) {
+   if (bike.weight < lightBicycle) {
+       lightBicycle = bike;
+   }
+}
+const { name, weight } = lightBicycle;
+console.log(`la bicicletta piu leggera è ${name}, pesa ${weight}kg`)
+*/
 
 
 
 
+/*
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà: punti fatti e 
+falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i 
+cui elementi contengono 
+solo nomi e falli subiti e stampiamo tutto in console.
 
+*/
+
+
+//Snack 4
+
+//1 creo un array di oggetti di squadre di calcio
+const squads = [
+    {
+        name: 'roma',
+        points: 0,
+        foulsImmediately: 0
+    },
+    {
+        name: 'milan',
+        points: 0,
+        foulsImmediately: 0
+    },
+    {
+        name: 'juventus',
+        points: 0,
+        foulsImmediately: 0
+    },
+    {
+        name: 'italia',
+        points: 0,
+        foulsImmediately: 0
+    },
+    {
+        name: 'argentina',
+        points: 0,
+        foulsImmediately: 0
+    },
+    {
+        name: 'brasile',
+        points: 0,
+        foulsImmediately: 0
+    },
+
+]
+
+
+//2 genero un numero random per assegnarlo ai punti e falli
+
+const getRandomNumbers = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+for (let squad of squads) {
+    squad.points = getRandomNumbers(1, 100);
+    squad.foulsImmediately = getRandomNumbers(1, 100);
+}
+
+
+const squadFouls = squads.map((squad) => `${squad.points} ${squad.foulsImmediately}`);
+console.log(squadFouls)
